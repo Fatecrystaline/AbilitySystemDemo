742e5af90c04538eca7e91344cad1644e64d7aa1
Fatecrystaline <Twistedjmonko@gmail.com>
2026-02-09 12:58:14 -0800

    Update Stab.h

--- AbilitySystemDemo MK.1/Stab.h
+++ AbilitySystemDemo MK.1/Stab.h
@@ -1,7 +1,26 @@
 #pragma once
 #include "Ability.h"
 
-class Stab : public Ability {
+/*
+    Stab (Ability)
+
+    Skeleton ability.
+
+    Damage Formula:
+        (Strength * 2) / Target Armor
+
+    No additional effects.
+*/
+
+class Stab : public Ability
+{
 public:
+    Stab()
+    {
+        cooldownDuration = 3.0f;
+    }
+
+    std::string GetName() const override { return "Stab"; }
+
     void Use(Entity& user, Entity& target) override;
 };
