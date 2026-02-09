1ead241a7d9aa13bf18bf2e43c68342764971dbc
Fatecrystaline <Twistedjmonko@gmail.com>
2026-02-09 12:58:38 -0800

    Update Stab.cpp

--- AbilitySystemDemo MK.1/Stab.cpp
+++ AbilitySystemDemo MK.1/Stab.cpp
@@ -5,9 +5,12 @@
 void Stab::Use(Entity& user, Entity& target)
 {
     int damage = (user.GetStrength() * 2) / target.GetArmor();
+    target.TakeDamage(damage);
 
-    target.ApplyDamage(damage, "physical");
+    CombatLog::Log(target.GetName() + " takes " +
+                   std::to_string(damage) +
+                   " physical damage (HP: " +
+                   std::to_string(target.GetHP()) + ")");
 
-    currentCooldown = cooldown;
+    StartCooldown();
 }
-
